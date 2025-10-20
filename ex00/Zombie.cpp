c1de84510f8b1ef6ff19243998f9aafd5cf6e107
hosu-kim <hosu@outlook.cz>
2025-10-20 21:37:00 +0900

    Add Zombie class implementation with constructor and announce method

--- ex00/Zombie.cpp
+++ ex00/Zombie.cpp
@@ -1,13 +1,21 @@
-#include <iostream>
-#include <string>
 #include "Zombie.hpp"
 
-Zombie::Zombie(std::string name): name(name)
-// 생성자 정의
-{
+Zombie::Zombie() {
 }
 
-void Zombie::announce(void)
-{
+Zombie::Zombie(std::string name): name(name) {
+// 생성자 정의
+}
+
+void Zombie::announce(void) {
 	std::cout << this->name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
+
+Zombie* newZombie(std::string name) {
+	return new Zombie(name);
+}
+
+void randomChump(std::string name) {
+	Zombie a_zombie(name);
+	a_zombie.announce();
 }
